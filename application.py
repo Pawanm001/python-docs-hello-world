1a31f931c43ea9cf5d04b9c8e03dfab41cc9cdbb
Pawan Mishra <pawanm001@gmail.com>
2020-04-29 13:08:30 +0530

    Update application.py

--- application.py
+++ application.py
@@ -2,13 +2,6 @@ from flask import Flask
 from flask import Flask,request,jsonify
 import pandas as pd
 import os
-from flask_cors import CORS, cross_origin
-path = os.getcwd()
-app = Flask(__name__)
-path = os.getcwd()
-
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
 app = Flask(__name__)
 
 @app.route("/")
@@ -17,9 +10,9 @@ def hello():
 
 
 @app.route('/assurent', methods=['POST'])
-@cross_origin(origin='*',headers=['Content-Type'])
 def TradeIn():  
+        text_data = request.form.get("data")
         tradeInOptions = pd.read_csv("TradeOptions.csv")
         tradeIn = tradeInOptions["TradeOptions"]  
         tradeInList = [x for x in tradeIn if str(x) != 'nan']
-        return "tradeInList"
+        return jsonify(tradeInList)
